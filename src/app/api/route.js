54487141af1a7cdc3453c1c348a8d264bdd40d06
crapthings <crapthings@gmail.com>
2025-05-16 11:00:14 +0800

    update

--- src/app/api/route.js
+++
@@ -1,9 +0,0 @@
-import { insertOnePost } from '@/lib/mongo'
-
-export async function POST (request) {
-  const post = await request.json()
-  console.log(post)
-  post.createdAt = new Date()
-  await insertOnePost('posts', post)
-  return new Response('OK', { status: 200 })
-}
